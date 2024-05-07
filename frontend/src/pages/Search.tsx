b25836c9bf3e0ec0d119c8c2aab9950222a9f14f
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-05-07 22:44:18 +0530

    search page completed

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,8 +1,93 @@
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [category, setCategory] = useState("");
+  const [page, setPage] = useState(1);
 
-export default Search
+  const addTocartHandeler = () => {};
+
+  const isPrevpage = page > 1;
+  const isNextpage = page < 4;
+
+  return (
+    <div className="product-search-page">
+      <aside>
+        <h2>Filters</h2>
+        <div>
+          <h4>Sort</h4>
+          <select value={sort} onChange={(e) => setSort(e.target.value)}>
+            <option value="">None</option>
+            <option value="asc">Price(Low to High)</option>
+            <option value="dsc">Price (High to Low)</option>
+          </select>
+        </div>
+        <div>
+          <h4>MaxPrice : {maxPrice || ""}</h4>
+          <input
+            type="range"
+            value={maxPrice}
+            max={100000}
+            min={100}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+        </div>
+
+        <div>
+          <h4>Category</h4>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <option value="">All</option>
+            <option value="">sample1</option>
+            <option value="">sample2</option>
+          </select>
+        </div>
+      </aside>
+      <main>
+        <h1>Products</h1>
+        <input
+          type="text"
+          placeholder="search by name..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <div className="search-product-list">
+          <ProductCard
+            productId="sad"
+            name="macbook"
+            price={2132311}
+            stock={12}
+            handeler={addTocartHandeler}
+            photo="https://m.media-amazon.com/images/I/61RJn0ofUsL._AC_UY218_.jpg"
+          />
+        </div>
+
+        <article>
+          <button
+            disabled={!isPrevpage}
+            onClick={() => setPage((prev) => prev - 1)}
+          >
+            Prev
+          </button>
+          <span>
+            {page} of {4}
+          </span>
+          <button
+            disabled={!isNextpage}
+            onClick={() => setPage((prev) => prev + 1)}
+          >
+            Next
+          </button>
+        </article>
+      </main>
+    </div>
+  );
+};
+
+export default Search;
