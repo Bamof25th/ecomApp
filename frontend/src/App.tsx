094ca0d8252e62e7a8d0b56d31604f2ad4587775
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-05-08 23:41:45 +0530

    Order Details page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Suspense, lazy } from "react";
 import Loader from "./components/Loader";
 import Header from "./components/Header";
 
+
 const Home = lazy(() => import("./pages/Home"));
 const Search = lazy(() => import("./pages/Search"));
 const Cart = lazy(() => import("./pages/Cart"));
 const Shipping = lazy(() => import("./pages/Shipping"));
 const Login = lazy(() => import("./pages/Login"));
-
+const Orders = lazy(() => import("./pages/Orders"));
+const OrderDetails = lazy(() => import("./pages/OrderDetails"));
 
 // * admin routes importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -44,6 +46,8 @@ const App = () => {
           {/* Logged in user routes */}
           <Route>
             <Route path="/shipping" element={<Shipping />} />
+            <Route path="/orders" element={<Orders />} />
+            <Route path="/order/:id" element={<OrderDetails />} />
           </Route>
           {/* Admin Routes */}
           // Rotues
---
+++ frontend/src/pages/Orders.tsx
@@ -0,0 +1,68 @@
+import { ReactElement, useState } from "react";
+import TableHOC from "../components/admin/TableHOC";
+import { Column } from "react-table";
+import { Link } from "react-router-dom";
+
+type DataType = {
+  _id: string;
+  amount: number;
+  quantity: number;
+  discount: number;
+  status: ReactElement;
+  action: ReactElement;
+};
+
+const column: Column<DataType>[] = [
+  {
+    Header: "ID",
+    accessor: "_id",
+  },
+  {
+    Header: "Quantity",
+    accessor: "quantity",
+  },
+  {
+    Header: "Discount",
+    accessor: "discount",
+  },
+  {
+    Header: "Amount",
+    accessor: "amount",
+  },
+  {
+    Header: "Status",
+    accessor: "status",
+  },
+  {
+    Header: "Action",
+    accessor: "action",
+  },
+];
+const Orders = () => {
+  const [rows] = useState<DataType[]>([
+    {
+      _id: "asdasd",
+      amount: 10002,
+      quantity: 32,
+      discount: 434,
+      status: <span className="red">Shipping</span>,
+      action: <Link to={`/order/asdasd`}>View</Link>,
+    }
+  ]);
+
+  const Table = TableHOC<DataType>(
+    column,
+    rows,
+    "dashbpard-product-box",
+    "Orders",
+     rows.length < 6  ? false : true
+  )();
+  return (
+    <div className="container">
+      <h1>My Orders</h1>
+      {Table}
+    </div>
+  );
+};
+
+export default Orders;
