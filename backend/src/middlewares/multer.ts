a139f708550f60ff58d6e314467a5364570e1cd2
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-05-12 23:17:06 +0530

    setting up multer done✅

--- backend/src/middlewares/multer.ts
+++ backend/src/middlewares/multer.ts
@@ -1,4 +1,12 @@
 import multer from "multer";
 
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
 
-multer().single("photo")
+export const singleUpload = multer({ storage }).single("photo");
