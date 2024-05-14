872697aa49a6e19d9eb2a8cb030a4094f5e90b5a
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-05-15 00:00:40 +0530

    invalidateCache funtoin on features

--- backend/src/types/types.ts
+++ backend/src/types/types.ts
@@ -39,3 +39,9 @@ export interface BaseQuery {
   };
   category?: string;
 }
+
+export type InvalidateCacheProps = {
+  product?: boolean;
+  order?: boolean;
+  admin?: boolean;
+};
--- backend/src/utils/features.ts
+++ backend/src/utils/features.ts
@@ -1,10 +1,39 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+import { InvalidateCacheProps } from "../types/types.js";
+import { myCache } from "../app.js";
+import { Product } from "../models/product.js";
 const url = "mongodb://127.0.0.1:27017";
 export const connectDB = () => {
+  mongoose
+    .connect(url, {
+      dbName: "Ecomdb_24",
+    })
+    .then((c) => console.log(`DB connected to ${c.connection.host}`))
+    .catch((e) => console.log(e));
+};
 
-    mongoose.connect(url, {
-        dbName: "Ecomdb_24"
-    }).then(c => console.log(`DB connected to ${c.connection.host}`))
-        .catch((e) => console.log(e));
+export const invalidateCache = async ({
+  product,
+  order,
+  admin,
+}: InvalidateCacheProps) => {
+  if (product) {
+    const productKeys: string[] = [
+      "latest-product",
+      "categories",
+      "all-product",
+    ];
+    // `product-${id}`
 
-}
+    const products = await Product.find({}).select("_id");
+    products.forEach((e) => {
+      productKeys.push(`product-${e._id}`);
+    });
+
+    myCache.del(productKeys);
+  }
+  if (order) {
+  }
+  if (admin) {
+  }
+};
