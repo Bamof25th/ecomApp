cd3dada8ce5755aecdf8a294e9fc61caa31ae85f
Bamof25th <88363603+Bamof25th@users.noreply.github.com>
2024-05-16 14:01:18 +0530

    coupon fuctoinalites complete

--- backend/src/controllers/payment.ts
+++ backend/src/controllers/payment.ts
@@ -1,3 +1,55 @@
 import { TryCatch } from "../middlewares/error.js";
+import { Coupon } from "../models/coupon.js";
+import ErrorHandeler from "../utils/utility-class.js";
 
-export const newCoupon = TryCatch(async (req, res, next) => {});
+export const newCoupon = TryCatch(async (req, res, next) => {
+  const { coupon, amount } = req.body;
+
+  if (!coupon || !amount)
+    return next(new ErrorHandeler("Please enter both Coupon and Amount", 404));
+
+  await Coupon.create({
+    code: coupon,
+    amount,
+  });
+
+  return res.status(201).json({
+    success: true,
+    message: `Coupon ${coupon} Created Successfully`,
+  });
+});
+
+export const applyDiscount = TryCatch(async (req, res, next) => {
+  const { coupon } = req.query;
+
+  const discount = await Coupon.findOne({ code: coupon });
+
+  if (!discount) return next(new ErrorHandeler("Invalid Coupon Code", 401));
+
+  return res.status(200).json({
+    success: true,
+    discount: discount.amount,
+  });
+});
+
+export const allCoupons = TryCatch(async (req, res, next) => {
+  const coupons = await Coupon.find({});
+
+  return res.status(200).json({
+    success: true,
+    coupons,
+  });
+});
+
+export const deleteCoupon = TryCatch(async (req, res, next) => {
+  const { id } = req.params;
+
+  const coupon = await Coupon.findByIdAndDelete(id);
+
+  if (!coupon) return next(new ErrorHandeler("Invalid Coupon Id", 400));
+
+  return res.status(200).json({
+    success: true,
+    message: `Coupon ${coupon?.code} Deleted Successfully`,
+  });
+});
--- backend/src/routes/payment.ts
+++ backend/src/routes/payment.ts
@@ -1,11 +1,23 @@
 import express from "express";
-import { newCoupon } from "../controllers/payment.js";
-
+import {
+  allCoupons,
+  applyDiscount,
+  deleteCoupon,
+  newCoupon,
+} from "../controllers/payment.js";
 
 const app = express.Router();
+
+// Route - /api/v1/payment/discount
+app.get("/discount", applyDiscount);
 
 // Route - /api/v1/payment/coupon/new
 app.post("/coupon/new", newCoupon);
 
+// Route - /api/v1/payment/coupon/all
+app.get("/coupon/all", allCoupons);
+
+// Route - /api/v1/payment/coupon/:id
+app.delete("/coupon/:id", deleteCoupon);
 
 export default app;
